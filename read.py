14d33442a543c90fccb3204fbc5b8af13cb03177
amber <hsinyuhuang2@google.com>
2019-05-24 22:58:51 +0800

    add3

--- read.py
+++ read.py
@@ -12,3 +12,10 @@ sl = 0
 for line in data:
 	sl = sl + len(line)
 print('平均長度為', sl/len(data),'！')
+
+new = []
+for line in data:
+	if len(line) < 100:
+		new.append(line)
+print('一共有', len(new), '筆資料長度小於100')
+print(new[0])
