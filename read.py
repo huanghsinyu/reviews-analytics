139bc52a4c13abde60ac08c4cd374da0172f9a73
amber <hsinyuhuang2@google.com>
2019-06-02 01:06:29 +0800

    new

--- read.py
+++ read.py
@@ -8,6 +8,8 @@ with open('reviews.txt', 'r')as f:
 			print(len(data))
 print('檔案讀取完了，總共有', len(data), '筆資料！')
 
+print(data[0])
+
 sl = 0
 for line in data:
 	sl = sl + len(line)
@@ -19,3 +21,35 @@ for line in data:
 		new.append(line)
 print('一共有', len(new), '筆資料長度小於100')
 print(new[0])
+
+g = [line for line in data if 'good' in line]
+print(g[0])
+print('一共有', len(g), '筆提到good')
+
+#文字記數
+
+wc = {}
+for d in data:
+	words = d.split(' ')
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+
+# print(len(wc))
+# print(wc['Amber'])
+
+# for word in wc:
+# 	if wc[word] > 100:
+# 		print(word, wc[word])
+
+while True:
+	word = input('請問你想查什麼字？')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過的次數為：', wc[word])
+	else:
+		print('這個字沒出現過喔！')
+print('感謝使用本功能')
